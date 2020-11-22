d8f339554ad79b0faf6ff93edf6d699a77c25f83
GilbertHsu <gilberthsu123@gmail.com>
2020-11-22 22:20:36 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -33,7 +33,6 @@ public class Account extends JComponent {
             frame.setSize(300, 150);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.add(panel);
-
             frame.setLocation(500,250);
 
             JLabel usernameLabel = new JLabel("username");
@@ -52,80 +51,82 @@ public class Account extends JComponent {
             panel.add(create);
 
             frame.setVisible(true);
-            create.addActionListener(actionListener);
-    }
-    ActionListener actionListener = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (new File("Data.txt").exists()) {
-                StringBuilder sb = new StringBuilder();
-                try {
-                    FileReader fr = new FileReader("Data.txt");
-                    BufferedReader bfr = new BufferedReader(fr);
-                    String line = bfr.readLine();
-                    while (line != null) {
-                        allUserAccounts.add(new Account(line.split(" ")[0],
-                                line.split(" ")[1]));
-                        accountsData.put(line.split(" ")[0],
-                                line.split(" ")[1]);
-                        line = bfr.readLine();
-                    }
-                    bfr.close();
-                } catch (IOException i) {
-                    i.printStackTrace();
-                }
-            }
-            if (e.getSource() == create ) {
-                if (!accountsData.containsKey(usernameText.getText())
-                        && usernameText.getText().matches("^[a-zA-Z0-9]*$")
-                        && passwordText.getText().length() >= 8
-                        && passwordText.getText().matches("^[a-zA-Z0-9]*$")) {
-                    accountsData.put(usernameText.getText(), passwordText.getText());
-                    allUserAccounts.add(new Account(usernameText.getText(),
-                            passwordText.getText()));
-                    if (!new File("Data.txt").exists()) {
-                        File toWrite = new File("Data.txt");
-                        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toWrite))) {
-                            StringBuilder sb = new StringBuilder();
-                            sb.append(allUserAccounts.get(allUserAccounts.size() - 1).username + " "
-                                    + allUserAccounts.get(allUserAccounts.size() - 1).password);
-                            bw.write(sb.toString());
-                            bw.newLine();
-                            bw.flush();
-                        } catch (IOException n) {
-                            n.printStackTrace();
-                            return;
-                        }
-                    } else if (new File("Data.txt").exists()) {
+            create.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (new File("Data.txt").exists()) {
+                        StringBuilder sb = new StringBuilder();
                         try {
-                            Files.write(Paths.get("Data.txt"), (usernameText.getText()
-                                    + " " + passwordText.getText() + "\n").getBytes(), StandardOpenOption.APPEND);
-                        } catch (IOException io ) {
-                            io.printStackTrace();
+                            FileReader fr = new FileReader("Data.txt");
+                            BufferedReader bfr = new BufferedReader(fr);
+                            String line = bfr.readLine();
+                            while (line != null) {
+                                allUserAccounts.add(new Account(line.split(" ")[0],
+                                        line.split(" ")[1]));
+                                accountsData.put(line.split(" ")[0],
+                                        line.split(" ")[1]);
+                                line = bfr.readLine();
+                            }
+                            bfr.close();
+                        } catch (IOException i) {
+                            i.printStackTrace();
                         }
                     }
-                } else if (!usernameText.getText().matches("^[a-zA-Z0-9]*$")) {
-                    JOptionPane.showMessageDialog(null, "Your username should be alphanumeric",
-                            "Profile app", JOptionPane.ERROR_MESSAGE);
-                    usernameText.setText("");
-                    passwordText.setText("");
-                } else if ( passwordText.getText().length() <= 8
-                        || !passwordText.getText().matches("^[a-zA-Z0-9]*$")) {
-                    JOptionPane.showMessageDialog(null, "Your password should be alphanumeric. Also, the length can't be fewer than 8 characters.",
-                            "Profile app", JOptionPane.ERROR_MESSAGE);
-                    usernameText.setText(usernameText.getText());
-                    passwordText.setText("");
-                } else if (accountsData.containsKey(usernameText.getText())) {
-                    JOptionPane.showMessageDialog(null, "This username has been used!",
-                            "Profile app",
-                            JOptionPane.ERROR_MESSAGE);
-                    usernameText.setText("");
-                    passwordText.setText("");
+                    if (e.getSource() == create ) {
+                        if (!accountsData.containsKey(usernameText.getText())
+                                && usernameText.getText().matches("^[a-zA-Z0-9]*$")
+                                && passwordText.getText().length() >= 8
+                                && passwordText.getText().matches("^[a-zA-Z0-9]*$")) {
+                            accountsData.put(usernameText.getText(), passwordText.getText());
+                            allUserAccounts.add(new Account(usernameText.getText(),
+                                    passwordText.getText()));
+                            if (!new File("Data.txt").exists()) {
+                                File toWrite = new File("Data.txt");
+                                try (BufferedWriter bw = new BufferedWriter(new FileWriter(toWrite))) {
+                                    StringBuilder sb = new StringBuilder();
+                                    sb.append(allUserAccounts.get(allUserAccounts.size() - 1).username + " "
+                                            + allUserAccounts.get(allUserAccounts.size() - 1).password);
+                                    bw.write(sb.toString());
+                                    bw.newLine();
+                                    bw.flush();
+                                } catch (IOException n) {
+                                    n.printStackTrace();
+                                    return;
+                                }
+                            } else if (new File("Data.txt").exists()) {
+                                try {
+                                    Files.write(Paths.get("Data.txt"), (usernameText.getText()
+                                            + " " + passwordText.getText() + "\n").getBytes(), StandardOpenOption.APPEND);
+                                } catch (IOException io ) {
+                                    io.printStackTrace();
+                                }
+                            }
+                            frame.dispose();
+                            JOptionPane.showMessageDialog(null, "Sign up successfully!", "Profile app",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        } else if (!usernameText.getText().matches("^[a-zA-Z0-9]*$")) {
+                            JOptionPane.showMessageDialog(null, "Your username should be alphanumeric",
+                                    "Profile app", JOptionPane.ERROR_MESSAGE);
+                            usernameText.setText("");
+                            passwordText.setText("");
+                        } else if ( passwordText.getText().length() <= 8
+                                || !passwordText.getText().matches("^[a-zA-Z0-9]*$")) {
+                            JOptionPane.showMessageDialog(null, "Your password should be alphanumeric. Also, the length can't be fewer than 8 characters.",
+                                    "Profile app", JOptionPane.ERROR_MESSAGE);
+                            usernameText.setText(usernameText.getText());
+                            passwordText.setText("");
+                        } else if (accountsData.containsKey(usernameText.getText())) {
+                            JOptionPane.showMessageDialog(null, "This username has been used!",
+                                    "Profile app",
+                                    JOptionPane.ERROR_MESSAGE);
+                            usernameText.setText("");
+                            passwordText.setText("");
+                        }
+                    }
                 }
-            }
-        }
-    };
+            });
 
+    }
     public String getPassword() {
         return password;
     }
