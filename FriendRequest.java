cb5a6070ce3e246d2e39041986a2c818d72d4ebd
Jin Lee <70168597+jinl33@users.noreply.github.com>
2020-11-30 15:17:50 +0900

    Added comments

--- FriendRequest.java
+++ FriendRequest.java
@@ -20,24 +20,30 @@ public class FriendRequest implements Serializable {
     String usernameWhoReceive;
     int status; // -1: refuese; 0: pending; 1: accepted
     
+    // creates a FriendRequest object with the usernames of the sender and the recipient
+    // sets the request status to pending
     public FriendRequest(String usernameWhoSent, String usernameWhoReceive) {
         this.usernameWhoSent = usernameWhoSent;
         this.usernameWhoReceive = usernameWhoReceive;
         this.status = 0;
     }
 
+    // returns the recipient's username
     public String getUsernameWhoReceive() {
         return usernameWhoReceive;
     }
 
+    // returns the sender's username
     public String getUsernameWhoSent() {
         return usernameWhoSent;
     }
 
+    // returns the request status
     public int getStatus() {
         return status;
     }
 
+    // updates the request status
     public void setStatus(int status) {
         this.status = status;
     }
