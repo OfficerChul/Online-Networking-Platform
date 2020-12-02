6968b2ec00d58e4c3369b65ad967f5bde7b4e405
GilbertHsu <gilberthsu123@gmail.com>
2020-12-02 16:54:13 +0800

    Update ProfileTest.java

--- ProfileTest.java
+++ ProfileTest.java
@@ -13,6 +13,9 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class ProfileTest {
@@ -759,6 +762,81 @@ public class ProfileTest {
 
         assertEquals("Fields didn't match", field.get(newProfile), newFriendRequestArray);
 
+    }
+    
+    /**
+     * The test below are testing whether the methods are workable
+     */
+        Profile profileEx = new Profile("",new Account("","")
+            ,"","","", new String[]{""});
+
+    @Test
+    void setName() {
+        profileEx.setName("name");
+        String expected = "name";
+        String actual = profileEx.getName();
+        assertEquals(expected,actual);
+        fail();
+    }
+
+    @Test
+    void setAboutMe() {
+        profileEx.setAboutMe("about me");
+        String expected = "about me";
+        String actual = profileEx.getAboutMe();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void setEmail() {
+        profileEx.setEmail("email");
+        String expected = "email";
+        String actual = profileEx.getEmail();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setFriendUserNames() {
+        profileEx.setFriendUserNames(new String[] {"friendOne", "friendTwo", "friendThree"});
+        String expected = new String[] {"friendOne", "friendTwo", "friendThree"}.toString();
+        String actual = profileEx.getFriendUserNames().toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setLikesAndInterests() {
+        profileEx.setLikesAndInterests("like and interest");
+        String expected = "like and interest";
+        String actual = profileEx.getLikesAndInterests();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setReceivedFriendRequests() {
+        FriendRequest[] friendRequestEx = new FriendRequest[1];
+        friendRequestEx[0] = new FriendRequest("sent", "receive");
+        profileEx.setReceivedFriendRequests(friendRequestEx);
+        String expected = "receive";
+        String actual = profileEx.getReceivedFriendRequests()[0].getUsernameWhoReceive();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setSentFriendRequests() {
+        FriendRequest[] friendRequestEx = new FriendRequest[1];
+        friendRequestEx[0] = new FriendRequest("sent", "receive");
+        profileEx.setReceivedFriendRequests(friendRequestEx);
+        String expected = "sent";
+        String actual = profileEx.getReceivedFriendRequests()[0].getUsernameWhoSent();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addToFriendUsernames() {
+        profileEx.addToFriendUsernames("usernameAdd");
+        String expected = "usernameAdd";
+        String actual = profileEx.getFriendUserNames()[profileEx.getFriendUserNames().length - 1];
+        assertEquals(expected, actual);
     }
 
 //    @Test
