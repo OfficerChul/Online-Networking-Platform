5e93e010f20097cc1b51be2f6ebf1c0a362d9cc6
Jin Lee <70168597+jinl33@users.noreply.github.com>
2020-12-07 20:37:52 +0900

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -4,17 +4,14 @@ import java.util.Arrays;
 /**
  * PJ05 Option 2 - Profile Class
  *
+ * Used to create a Profile object and return and set its parameters
+ * 
  * @author Kyochul Jang
  * @version November 23, 2020
  */
 
 public class Profile implements Serializable {
-
-    /**
-     *
-     */
     private static final long serialVersionUID = 2395638123565212616L;
-
 
     // userName should be unique for the Account object
     private Account account;
@@ -26,7 +23,7 @@ public class Profile implements Serializable {
     private FriendRequest[] sentFriendRequests;
     private FriendRequest[] receivedFriendRequests;
 
-    // Constructs the Profile object
+    // Constructs the Profile object with the given parameters
     public Profile(String name, Account account, String email,
                    String aboutMe, String likesAndInterests, String[] friendUserNames) {
         this.name = name;
@@ -40,154 +37,170 @@ public class Profile implements Serializable {
     }
     
     
-    /** 
+    /**
+     * return an Account object
+     * 
      * @return Account
      */
-    // return an Account object
     public Account getAccount() {
         return account;
     }
 
     
-    /** 
+    /**
+     * return an array of friend requests received from other users
+     * 
      * @return FriendRequest[]
      */
-    // return an array of friend requests received from other users
     public FriendRequest[] getReceivedFriendRequests() {
         return receivedFriendRequests;
     }
 
     
-    /** 
+    /**
+     * return an array of friend requests sent to other users
+     * 
      * @return FriendRequest[]
      */
-    // return an array of friend requests sent to other users
     public FriendRequest[] getSentFriendRequests() {
         return sentFriendRequests;
     }
 
     
-    /** 
+    /**
+     * return a String array of usernames of friended users
+     * 
      * @return String[]
      */
-    // return a String array of usernames of friended users
     public String[] getFriendUserNames() {
         return friendUserNames;
     }
 
     
-    /** 
+    /**
+     * return user's likes and interests
+     * 
      * @return String
      */
-    // return user's likes and interests
     public String getLikesAndInterests() {
         return likesAndInterests;
     }
 
     
-    /** 
+    /**
+     * return user's "About Me"
+     * 
      * @return String
      */
-    // return user's "About Me"
     public String getAboutMe() {
         return aboutMe;
     }
 
     
-    /** 
+    /**
+     * return user's email address 
+     *
      * @return String
      */
-    // return user's email address
     public String getEmail() {
         return email;
     }
 
     
-    /** 
+    /**
+     * return user's name
+     * 
      * @return String
      */
-    // return user's name
     public String getName() {
         return name;
     }
 
     
-    /** 
+    /**
+     * updates user's name with the given name
+     * 
      * @param name
      */
-    // updates user's name with the given name
     public void setName(String name) {
         this.name = name;
     }
 
     
-    /** 
+    /**
+     * updates user's "About Me" page with the given string of texts
+     *
      * @param aboutMe
      */
-    // updates user's "About Me" page with the given string of texts
     public void setAboutMe(String aboutMe) {
         this.aboutMe = aboutMe;
     }
 
     
-    /** 
+    /**
+     * updates the user's account
+     * 
      * @param account
      */
-    // updates the user's account
     public void setAccount(Account account) {
         this.account = account;
     }
 
     
-    /** 
+    /**
+     * updates email
+     * 
      * @param email
      */
-    // updates email
     public void setEmail(String email) {
         this.email = email;
     }
 
     
-    /** 
+    /**
+     * updates friendlist of usernames
+     *
      * @param friendUserNames
      */
-    // updates friendlist of usernames
     public void setFriendUserNames(String[] friendUserNames) {
         this.friendUserNames = friendUserNames;
     }
 
     
-    /** 
+    /**
+     * updates likes and interests page
      * @param likesAndInterests
      */
-    // updates likes and interests page
     public void setLikesAndInterests(String likesAndInterests) {
         this.likesAndInterests = likesAndInterests;
     }
 
     
-    /** 
+    /**
+     * updates an array of received friend requests
+     *
      * @param receivedFriendRequests
      */
-    // updates an array of received friend requests
     public void setReceivedFriendRequests(FriendRequest[] receivedFriendRequests) {
         this.receivedFriendRequests = receivedFriendRequests;
     }
 
     
-    /** 
+    /**
+     * updates an array of sent friend requests
+     *
      * @param sentFriendRequests
      */
-    // updates an array of sent friend requests
     public void setSentFriendRequests(FriendRequest[] sentFriendRequests) {
         this.sentFriendRequests = sentFriendRequests;
     }
 
     
-    /** 
+    /**
+     * adds a username to the friend list
+     *
      * @param username
      */
-    // adds a username to the friend list
     public void addToFriendUsernames(String username) {
         friendUserNames = Arrays.copyOf(friendUserNames, friendUserNames.length + 1);
         friendUserNames[friendUserNames.length - 1] = username;
